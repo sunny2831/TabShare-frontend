594678d949987a926d1d9894c0b857f3959c7c1c
github username <github email address>
2019-03-19 12:55:12 +0000

    added homepage styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import TabShareLogo from './images/TabShareLogo.png'
+import HomePageIntro from './images/HomePageIntro.png'
+import CoinBackground from './images/CoinBackground.png'
+import DemoPage from './images/DemoPage.png'
+
+
 
 class App extends Component {
 
     render() {
       return (
-        <header className='nav-bar'>
+        <main className='web-page-box'>
           <div className='nav-bar-box'>
-              <span className="Tabshare-header"> TabShare</span>
+              <img className="tab-share-logo" alt="main-logo" src={TabShareLogo} />
+              <button className="login-btn" type="button">Log in</button>
           </div>
-        </header>
+          <img className="homepage-intro" alt="main-intro" src={HomePageIntro} />
+          <img className="coin-bg" alt="coin-bg" src={CoinBackground} />
+          <img className="demo-page" alt="demo-page" src={DemoPage} />
+          <div className="signup-field">
+          </div>
+          <form >
+              <input type="text" placeholder="pick a username" className="username" required></input>
+              <input type="text" placeholder="your email address" className="email" required></input>
+              <input type="text" placeholder="create a password" className="password" required></input>
+              <button className="signup-btn" type="button">Sign Up for TabShare</button>
+          </form>
+        </main>
       );
     };
 }
